071ae5edd572c9b20462d81a796f6b634f6ec197
Teun Zengerink <t.zengerink@gmail.com>
2015-05-01 12:05:58 +0200

    added minimal test for list controller

---
+++ tests/groceries/list.controller.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('LoginController', function() {
+    var $controller;
+
+    beforeEach(module('groceries'));
+
+    beforeEach(inject(function($injector) {
+        $controller = $injector.get('$controller');
+    }));
+
+    it('should exist', function() {
+        var controller = $controller('ListController');
+        expect(controller).not.toBe(undefined);
+    });
+});
