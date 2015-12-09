2616b76b367b35d9675905d4d48e3972a01471b9
Teun Zengerink <t.zengerink@gmail.com>
2015-12-09 17:33:44 +0100

    modified how the module parts are required

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -5,15 +5,5 @@
     require('angular-route');
     require('angular-touch');
 
-    require('./groceries/config.js');
     require('./groceries/module.js');
-    require('./groceries/directives/auto-focus.js');
-    require('./groceries/directives/swipe-delete.js');
-    require('./groceries/services/groceries.js');
-    require('./groceries/services/localstorage.js');
-    require('./groceries/controllers/list.js');
-    require('./groceries/controllers/login.js');
-    require('./groceries/controllers/logout.js');
-    require('./groceries/controllers/menu.js');
-    require('./groceries/controllers/new.js');
 })();
